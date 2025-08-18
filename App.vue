5fafb13ba0647fd6771cb7a4602b86339b5e0e4a
13788480300 <610623429@qq.com>
2025-08-18 10:45:38 +0800

    add App

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <view>
+    <image src="/static/logo.png" style="width:100px;height:100px;" />
+    <router-view />
+  </view>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
