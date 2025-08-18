ad8ea510524704fce188a917beb8f8f33d2e2369
13788480300 <610623429@qq.com>
2025-08-18 09:41:06 +0800

    pages

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <view>
+    <group-list />
+    <chat-message v-for="msg in messages" :key="msg.id" :message="msg" />
+    <voice-button @tap="sendVoice" />
+  </view>
+</template>
+
+<script>
+import GroupList from '@/components/GroupList.vue'
+import ChatMessage from '@/components/ChatMessage.vue'
+import VoiceButton from '@/components/VoiceButton.vue'
+
+export default {
+  components: { GroupList, ChatMessage, VoiceButton },
+  data() {
+    return {
+      messages: []
+    }
+  },
+  methods: {
+    sendVoice() {
+      // handle voice sending
+    }
+  }
+}
+</script>
